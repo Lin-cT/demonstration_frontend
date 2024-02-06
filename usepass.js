7c8d7a14188b8dee6ac5fb12f374ec2b8d2c9b86
Lin-cT <lindsaytang8@gmail.com>
2024-02-06 03:25:17 -0800

    CRUD

--- usepass.js
+++ usepass.js
@@ -1,23 +1,44 @@
-// Function to retrieve JWT token from cookie
-function getJwtToken() {
-    const cookies = document.cookie.split(';');
-    for (let cookie of cookies) {
-        const [name, value] = cookie.trim().split('=');
-        if (name === 'jwtToken') {
-            return value;
+function login(){
+    // Set Authenticate endpoint
+    const url = 'http://127.0.0.1:8086/api/users/authenticate';
+
+    // Set the body of the request to include login data from the DOM
+    const body = {
+        uid: document.getElementById("uid").value,
+        password: document.getElementById("password").value,
+    };
+
+    // Change options according to Authentication requirements
+    const authOptions = {
+        mode: 'cors', // no-cors, *cors, same-origin
+        credentials: 'include', // include, same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'POST', // Override the method property
+        cache: 'no-cache', // Set the cache property
+        body: JSON.stringify(body)
+    };
+
+    // Fetch JWT
+    fetch(url, authOptions)
+    .then(response => {
+        // handle error response from Web API
+        if (!response.ok) {
+            const errorMsg = 'Login error: ' + response.status;
+            console.log(errorMsg);
+            alert("Failed Authentication: Credentials Incorrect")
+            return;
         }
-    }
-    return null;
+        // Success!!!
+        // Redirect to the database page
+        window.location.href = "/csp-blog//log/2024/01/30/Users.html";
+    })
+    // catch fetch errors (ie ACCESS to server blocked)
+    .catch(err => {
+        console.error(err);
+    });
 }
 
-// Function to attach JWT token to requests
-function attachJwtTokenToRequests() {
-    const jwtToken = getJwtToken();
-    if (jwtToken) {
-        // Attach JWT token to request headers
-        fetchOptions.headers.Authorization = `Bearer ${jwtToken}`;
-    } else {
-        // Redirect to login page if JWT token is missing
-        window.location.href = "https://lin-ct.github.io/demonstration_frontend/login.html";
-    }
-}
+// Attach login_user to the window object, allowing access to form action
+window.login_user = login_user;
